6e2dccdeac2a1c011efee6b1927cb01ac1a2a983
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2022-05-09 20:16:09 +0300

    Add files via upload

---
+++ fishTank.js
@@ -0,0 +1,13 @@
+function fishTank(input){
+
+    let lenght = Number(input[0]);
+    let width = Number(input[1]);
+    let height = Number(input[2]);
+    let precentageReserve = Number(input[3]);
+
+    let tankVolume = lenght * width * height;
+    let tankVolumeInDM = tankVolume * 0.001;
+    let total = tankVolumeInDM - (tankVolumeInDM * (precentageReserve / 100 ));
+
+    console.log(total)
+}
