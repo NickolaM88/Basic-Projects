0f2a2e9be09dc155eeba6160bb048682ce356d49
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2022-05-13 23:50:44 +0300

    Add files via upload

---
+++ petShop.js
@@ -0,0 +1,10 @@
+function petShop(input){
+
+    let dogfood = input[0] * 2.50;
+    let catfood = input[1] * 4;
+    let totalsum = dogfood + catfood;
+
+    console.log(`${totalsum} lv.`)
+}
+
+petShop([13 , 9])
