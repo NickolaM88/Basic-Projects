5bef4e0ce37a9d9ca4fd214004c9abae3d9e1022
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2022-05-21 23:44:25 +0300

    Add files via upload

---
+++ invalidNumber.js
@@ -0,0 +1,13 @@
+function invalidNumber(input) {
+
+    function invalidNumber(input) {
+        let num = Number(input[0])
+        if (num === 0 || num >= 100 && num <= 200) {
+
+        } else {
+            console.log("invalid");
+        }
+    }
+}
+
+
